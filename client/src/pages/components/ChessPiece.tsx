4b75ffb476e7c2986dbf2fbe3021165c784a37a6
Ajnur <radovic.ajnur96@gmail.com>
2023-02-06 23:37:09 +0100

    chessPiece

---
+++ client/src/pages/components/ChessPiece.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+
+
+
+const ChessPiece: React.FunctionComponent = () => {
+
+  
+
+  return (
+    <div>
+        
+    </div>
+  );
+};
+
+export default ChessPiece;
