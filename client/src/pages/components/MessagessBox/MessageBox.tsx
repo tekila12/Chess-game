9cdc7dbec9ce466ee1dac04165607324e9908721
Ajnur <radovic.ajnur96@gmail.com>
2023-03-17 23:00:57 +0100

    added cors exp

--- client/src/pages/components/MessagessBox/MessageBox.tsx
+++ client/src/pages/components/MessagessBox/MessageBox.tsx
@@ -26,6 +26,12 @@ const Index: React.FC = () => {
     if (process.env.BASE_URL) {
       socket = SocketIOClient(process.env.BASE_URL, {
         path: '/api/socket',
+        withCredentials: true,
+        extraHeaders: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization, x-id, Content-Length, X-Requested-With',
+        },
       });
       // ...
     } else {
