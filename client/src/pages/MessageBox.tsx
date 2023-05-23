a424ab6814c6bb1fbbe596c8c5ba7e0f53fee327
Ajnur <radovic.ajnur96@gmail.com>
2023-05-23 22:33:53 +0200

    props

--- client/src/pages/MessageBox.tsx
+++ client/src/pages/MessageBox.tsx
@@ -64,7 +64,7 @@ export default function MessageBox() {
         {!chosenUsername ? (
           <>
             <h3 >
-              How people should call you?
+              Enter your username?
             </h3>
             <input
               type="text"
